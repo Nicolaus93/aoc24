10b11f7073954fb522e51c74e158b2e7a556d899
nico <nico.campolongo@gmail.com>
2024-12-30 16:28:05 +0100

    Make it shorter

--- day19.py
+++ day19.py
@@ -1,24 +1,11 @@
+from functools import cache
 
-def count_compositions(main_string: str, smaller_strings: set[str]) -> int:
+@cache
+def count_subs(main_string: str):
+    if main_string == '': return 1
+    return sum(count_subs(main_string[len(s):]) for s in patterns if main_string.startswith(s))
 
-    # dp[i] stores the number of ways to compose the prefix main_string[:i] using the smaller strings
-    dp = [0] * (len(main_string) + 1)
-    dp[0] = 1  # Base case: one way to compose an empty string
-
-    # Iterate over all positions in main_string
-    # For each position i, check all substrings main_string[j:i] (from index j to i) to see if they exist in the set
-    # of smaller strings.
-    for i in range(1, len(main_string) + 1):
-        for j in range(i):
-            substring = main_string[j:i]
-            if substring in smaller_strings:
-                dp[i] += dp[j]  # Add the count from dp[j]
-
-    return dp[len(main_string)]
-
-
-ll = open("d19.txt", 'r').read().splitlines()
-ptt = {i for i in ll[0].split(", ")}
-comps = ll[2:]
-print(sum(1 for c in comps if count_compositions(c, ptt)))
-print(sum(count_compositions(c, ptt) for c in comps))
+lines = open("d19.txt", 'r').read().splitlines()
+patterns = {i for i in lines[0].split(", ")}
+print(sum(count_subs(c) > 0 for c in lines[2:]))
+print(sum(count_subs(c) for c in lines[2:]))
