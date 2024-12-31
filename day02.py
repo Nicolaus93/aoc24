bbe6737a93751d3388c42e519e53a6a9cf4ba2a5
nico <nico.campolongo@gmail.com>
2024-12-31 15:07:33 +0100

    make it shorter

--- day02.py
+++ day02.py
@@ -2,36 +2,10 @@
 import re
 from itertools import pairwise
 
+def solve():
+    lines = [[int(i) for i in re.findall(r'\d+', line)] for line in open("d2.txt").read().splitlines()]
+    is_safe = lambda x: all(i > 0 and abs(i) <= 3 for i in x) or all(i < 0 and abs(i) <= 3 for i in x)
+    print("part 1:", sum(is_safe([i - j for i, j in pairwise(nums)]) for nums in lines))
+    print("part 2:", sum((any((is_safe([j - k for j, k in pairwise([n for pos, n in enumerate(nums) if i != pos])]) for i in range(len(nums)))) for nums in lines)))
 
-def is_safe(diff):
-    if all(i > 0 and abs(i) <= 3 for i in diff):
-        return True
-    if all(i < 0 and abs(i) <= 3 for i in diff):
-        return True
-    return False
-
-
-def part_1():
-    with open("d2.txt", 'r') as f:
-        pattern = r'\b\d+\b'
-        safe = 0
-        safe_2 = 0
-        for line in f:
-            nums = [int(i) for i in re.findall(pattern, line)]
-            diff = [i - j for i, j in pairwise(nums)]
-            if is_safe(diff):
-                safe += 1
-            else:
-                for i in range(len(nums)):
-                    new_nums = [n for pos, n in enumerate(nums) if i != pos]
-                    diff = [i - j for i, j in pairwise(new_nums)]
-                    if is_safe(diff):
-                        safe_2 += 1
-                        break
-
-    print("part1:", safe)
-    print("part2:", safe_2 + safe)
-
-
-if __name__ == "__main__":
-    part_1()
+solve()
