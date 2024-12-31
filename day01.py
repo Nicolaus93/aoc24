0bd85ca27a74360ef905d2038f58c1da68f8d461
nico <nico.campolongo@gmail.com>
2024-12-31 14:18:41 +0100

    make it shorter

--- day01.py
+++ day01.py
@@ -1,37 +1,14 @@
 
-
 import re
 from collections import Counter
 
 
-def part_1():
-    with open("d1.txt", 'r') as f:
-        lines = f.readlines()
-        pattern = r'\b\d+\b'
-        n1 = []
-        n2 = []
-        for line in lines:
-            integer_numbers = re.findall(pattern, line)
-
-            # Convert the matched strings to integers
-            integer_numbers = [int(num) for num in integer_numbers]
-            n1.append(integer_numbers[0])
-            n2.append(integer_numbers[1])
-
-        n1 = sorted(n1)
-        n2 = sorted(n2)
-
+def solve():
+    nums = [[int(i) for i in re.findall(r'\d+', line)] for line in open("d1.txt").read().splitlines()]
+    n1 = sorted(n[0] for n in nums)
+    n2 = sorted(n[1] for n in nums)
     count = Counter(n2)
-    dist = 0
-    sim_score = 0
-    for m1, m2 in zip(n1, n2):
-        m1 = m1 * count[m1]
-        dist += abs(m1 - m2)
-        sim_score += m1
+    print("part1:", sum(abs(i * count[i] - j) for i, j in zip(n1, n2)))
+    print("part2:", sum(i * count[i] for i in n1))
 
-    print("part1:", dist)
-    print("part2:", sim_score)
-
-
-if __name__ == "__main__":
-    part_1()
+solve()
