6cd06b63d49c66a43756fd503a81816bf9917d61
nico <nico.campolongo@gmail.com>
2025-01-01 21:11:57 +0100

    fix typo

--- day07.py
+++ day07.py
@@ -1,5 +1,4 @@
 import re
-import sys
 from itertools import product
 from operator import add, mul
 from utils import progressbar
@@ -8,28 +7,23 @@ from utils import progressbar
 def solve(lines, is_part_2=False):
     ans = 0
     all_ops = [add, mul]
+    if is_part_2:
+        all_ops.append(lambda x, y: int(str(x) + str(y)))
+
     for line in progressbar(lines):
         n, *nums = [int(i) for i in re.findall(r'\d+', line)]
-        if is_part_2:
-            all_ops.append(lambda x, y: int(str(x) + str(y)))
-        for ops in product(all_ops, repeat=len(nums) - 1):
+        for pos, ops in enumerate(product(all_ops, repeat=len(nums) - 1)):
             result = nums[0]
-            for i, op in enumerate(ops):
-                result = op(result, nums[i + 1])
+            for op, next_n in zip(ops, nums[1:]):
+                result = op(result, next_n)
                 if result > n:
                     break
-
             if result == n:
                 ans += n
                 break
     return ans
 
 
-
-if __name__ == "__main__":
-    with open(sys.argv[1], 'r') as f:
-        ll = [line.strip() for line in f]
-
-    
-    print(solve(ll))
-    print(solve(ll, is_part_2=True))
+ll = open("d7.txt", 'r').read().splitlines()
+print(solve(ll))
+print(solve(ll, is_part_2=True))
