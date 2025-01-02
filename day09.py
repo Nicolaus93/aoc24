18f8395adc006687ba162170e8dac03cdd704b00
nico <nico.campolongo@gmail.com>
2025-01-02 10:23:45 +0100

    make it shorter

--- day09.py
+++ day09.py
@@ -1,141 +1,80 @@
-import sys
-import uuid
-from dataclasses import dataclass
-from itertools import pairwise
-
-from utils import progressbar
-from line_profiler_pycharm import profile
 from collections import defaultdict
+from itertools import pairwise
+from bisect import insort
 
 
-def sol(lines):
-    ans = 0
-    for line in lines:
-        expand = []
-        for pos, value in enumerate(line):
-            if pos % 2 == 0:
-                expand += [pos // 2] * int(value)
-            else:
-                expand += ['.'] * int(value)
-
-        left = 0
-        right = len(expand) - 1
-        while left < right:
-            if expand[left] != '.':
-                left += 1
-                continue
-            if expand[right] == '.':
-                right -= 1
-                continue
-            expand[left], expand[right] = expand[right], expand[left]
-            left += 1
-            right -= 1
-        ans += sum(pos * int(value) for pos, value in enumerate(expand) if value != '.')
-
-    return ans
-
-
-def part_3(lines):
-    ans = 0
-    mem = list(map(int, lines[0]))
-
-    return ans
-
-@profile
-def part_2(lines):
-
-    ans = 0
-    for line in lines:
-        expand = []
-        for pos, value in enumerate(line):
-            if pos % 2 == 0:
-                expand += [pos // 2] * int(value)
-            else:
-                expand += ['.'] * int(value)
-
-        it = [i for i in reversed(range(expand[-1] + 1))]
-        for i in progressbar(it):
-            count = expand.count(i)
-            r = expand.index(i)
-            l = count_point = 0
-            while l < r:
-                if expand[l] == '.':
-                    for count_point, value in enumerate(pairwise(expand[l:])):
-                        if value[0] != value[1]:
-                            break
-                    count_point += 1
-                    if count_point >= count:
-                        break
-                    l += count_point
-                else:
-                    l += 1
-
-            if count_point >= count:
-                expand[l:l + count], expand[r:r + count] = expand[r:r + count], expand[l:l + count]
-            # print(''.join((str(i) for i in expand)), i)
-
-        ans += sum(pos * int(value) for pos, value in enumerate(expand) if value != ".")
-
-    return ans
-
-
-@dataclass(frozen=True)
-class File:
-    size: int
-    id: int
-
-
-def solv(lines):
-    ans = 0
-    files = []
-    points = defaultdict(list)
-    for pos, value in enumerate(map(int, lines[0])):
+def expand_nums(block):
+    expand = []
+    for pos, value in enumerate(block):
         if pos % 2 == 0:
-            files.append(File(value, pos // 2))
+            expand += [pos // 2] * int(value)
         else:
-            points[value].append(pos // 2)
-
-    for f in files[::-1]:
-        print(f)
-
-    print(points)
-    return ans
+            expand += ['.'] * int(value)
+    return expand, pos // 2
 
 
-def solve(lines):
-    ans = 0
-    count = dict()
-    for line in lines:
-        expand = ''
-        for pos, value in enumerate(line):
-            if pos % 2 == 0:
-                expand += str(pos // 2) * int(value)
-                count[pos // 2] = int(value)
-            else:
-                expand += '.' * int(value)
+def get_free_sizes(mem):
+    free_sizes = defaultdict(list)
+    for pos, couple in enumerate(pairwise(mem)):
+        i, j = couple
+        if i != j and j == '.':
+            free_size = 1
+        elif i == j == '.':
+            free_size += 1
+        elif i != j and i == '.':
+            free_sizes[free_size].append(pos - free_size + 1)
+    return free_sizes
 
-        max_key = max(map(int, count.keys()))
-        right = len(expand) - 1
-        for i in reversed(range(max_key + 1)):
-            last = str(i)
-            count = count[i]
-            right -= count - 1
-            if expand[:right].find('.' * count) >= 0:
-                first = expand[:right].replace('.' * count, last * count, 1)
-                second = expand[right:].replace(last * count, '.' * count, 1)
-                expand = first + second
-                # print(expand)
-            while expand[right] != str(i - 1) and right > 0:
-                right -= 1
 
-        ans += sum(pos * int(value) for pos, value in enumerate(expand.replace('.', '0')))
-    return ans
+def part_1(lines):
+    expand, max_n = expand_nums(lines[0])
+    left = 0
+    right = len(expand) - 1
+    while left < right:
+        if expand[left] != '.':
+            left += 1
+            continue
+        if expand[right] == '.':
+            right -= 1
+            continue
+        expand[left], expand[right] = expand[right], expand[left]
+        left += 1
+        right -= 1
+    return sum(pos * int(value) for pos, value in enumerate(expand) if value != '.')
+
+
+def get_block_size(n, mem, r_idx):
+    l_idx = r_idx
+    while mem[l_idx] == n and l_idx >= 0:
+        l_idx -= 1
+    return l_idx + 1
+
+
+def part_2(disk_map):
+    mem, max_n = expand_nums(disk_map)
+    free_sizes = get_free_sizes(mem)
+    n = 9999
+    r = len(mem) - 1
+    while n > 0:
+        l = get_block_size(n, mem, r)
+        block_size = r + 1 - l
+        keys = [k for k in free_sizes.keys() if k >= block_size]
+        key = min(keys, key=lambda k: free_sizes[k][0], default=-1)
+        if key != -1:
+            pos = free_sizes[key].pop(0)
+            if not free_sizes[key]: del free_sizes[key]
+            if pos < r:
+                mem[pos:pos + block_size], mem[l:l + block_size] = mem[l:l + block_size], mem[pos:pos + block_size]
+                # update free sizes
+                if (left_space := key - block_size) > 0:
+                    insort(free_sizes[left_space], pos + block_size)
+        n -= 1
+        while 0 < n != mem[r]:
+            r -= 1
+    return sum(pos * int(value) for pos, value in enumerate(mem) if value != ".")
 
 
 if __name__ == "__main__":
-    with open(sys.argv[1], 'r') as f:
-        ll = [i.strip() for i in f.readlines()]
-
-    # print(sol(ll))
-    # print(part_2(ll))
-    print(solv(ll))
+    ll = open("d9.txt", 'r').read().splitlines()
+    print("part 1:", part_1(ll))
+    print("part 2:", part_2(ll[0]))
